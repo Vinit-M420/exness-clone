1ae634f032728675bc1f2e3f71b51fd1ddae8c56
Vinit <vinit_motghare@outlook.com>
2025-12-09 22:19:10 +0530

    init backend folder

---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  port: 4000,
+  fetch: app.fetch,
+//   websocket: app.websocket, // if using Hono WS helper
+};
+
